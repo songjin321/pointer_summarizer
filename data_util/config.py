3ce2bf8bf08ef76c1e3df5894d0756028ed24346
songjin321 <songjinxn@163.com>
2018-12-03 17:14:15 +0800

    fix path bug

--- data_util/config.py
+++ data_util/config.py
@@ -3,11 +3,11 @@ import os
 root_dir = os.path.expanduser("~")
 
 #train_data_path = os.path.join(root_dir, "ptr_nw/cnn-dailymail-master/finished_files/train.bin")
-train_data_path = os.path.join(root_dir, "../pointer_bert/finished_files/chunked/train_*")
-eval_data_path = os.path.join(root_dir, "../pointer_bert/finished_files/val.bin")
-decode_data_path = os.path.join(root_dir, "../pointer_bert/finished_files/test.bin")
-vocab_path = os.path.join(root_dir, "../pointer_bert/finished_files/vocab")
-log_root = os.path.join(root_dir, "../log")
+train_data_path = "../pointer_bert/finished_files/chunked/train_*"
+eval_data_path = "../pointer_bert/finished_files/val.bin"
+decode_data_path = "../pointer_bert/finished_files/test.bin"
+vocab_path = "../pointer_bert/finished_files/vocab"
+log_root =  "../pointer_bert/log"
 
 # Hyperparameters
 hidden_dim= 256
